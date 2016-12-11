6357041e84cbf30777ed21ce78d776fd4578454f
unknown <>
2016-12-11 20:48:55 +0900

    ng2 setting

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DocumentComponent } from '../document/document.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentComponent
   ],
   bootstrap: [AppComponent]
 })
